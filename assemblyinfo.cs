79952a9a305abb3cfd4ab15cae2ab54205aa6a12
Jim Lawless <lawless.jim@gmail.com>
2022-03-23 18:51:16 -0500

    Added version/copyright info.

---
+++ assemblyinfo.cs
@@ -0,0 +1,28 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MailWrench")]
+[assembly: AssemblyDescription("Command-line SMTP Mailer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("James K. Lawless")]
+[assembly: AssemblyProduct("MailWrench")]
+[assembly: AssemblyCopyright("Copyright © 2011-2022 by James K. Lawless")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//[assembly:TargetFrameworkAttribute(".NETFramework,Version=v4.5,Profile=Client", FrameworkDisplayName = ".NET Framework 4.5 Client Profile")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.10.0.0")]
+[assembly: AssemblyFileVersion("2.10.0.0")]
